054fb0b08a1f7b3726f5ab9021e00b5ebc876057
Jessica <j.moxam-fangmann@outlook.com>
2024-10-03 07:55:11 +0100

    starting on defer cat network graph

--- visuals.R
+++ visuals.R
@@ -1,4 +1,16 @@
 library(tidyverse)
 library(readr)
+library(igraph)
+library(networkD3)
 
 defer_cat_process <- read_csv("data/dummy_defer_cat_process.csv")
+defer_cat_process["prev_step"] <- defer_cat_process["step"] - 1
+
+# Network Graph ====
+edge_list <- inner_join(
+  defer_cat_process,
+  defer_cat_process,
+  by = c("step" = "prev_step"),
+  suffix = c("_from", "_to")
+) %>% 
+
